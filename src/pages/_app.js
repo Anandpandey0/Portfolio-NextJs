aec815a75f79c264cbd10eb07207b585b8183831
Anandpandey0 <75582022+Anandpandey0@users.noreply.github.com>
2023-05-10 09:30:32 +0530

    added analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps, session }) {
   return (
     <SessionProvider session={session}>
       <ChakraProvider>
         <Component {...pageProps} />
+        <Analytics />
       </ChakraProvider>
     </SessionProvider>
   );
